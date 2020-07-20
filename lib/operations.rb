0217ee43ad4f5d2d0879d4d5534f2d5e886b5e02
Valentina E <tiyahelfrih@gmail.com>
2020-07-20 12:58:23 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,18 @@
-def un_safe(speed)
-  if speed > 60
-     true
-    
-  elsif 
-  speed < 40
-true
-  
-else 
-false
-  
-  def not_safe?(speed)
-	speed < 40 || speed > 60 ? true : false
+def unsafe?(speed)
+	if speed > 60 
+		true
+	elsif 
+		speed < 40
+		true
+	else
+		false
 	end
-    
+
+end
+
+
+
+def not_safe?(speed)
+	speed < 40 || speed > 60 ? true : false
+
+end
